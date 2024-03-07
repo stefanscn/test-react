1c8eba4b7e56e8ea63b06d4f4c76c9e5b71d5714
SparkFlowGt <scinteiestefan@gmail.com>
2024-03-07 22:20:07 +0200

    adding outlet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Outlet } from "react-router-dom";
 
 import Home from "./components/routes/home/home.component";
 
+const Navigation = () => {
+   return (
+      <div>
+         <div>
+            <h1> I am the navigation bar </h1>
+         </div>
+         <Outlet/>
+      </div>
+   )
+}
+
+const Shop = () => {
+   return <h1> I am the shop page</h1>;
+};
+
 const App = () => {
 return( 
-   <Routes >
-      <Route path = '/' element ={<Home/>}></Route>
+   <Routes>
+   <Route path='/' element={<Navigation/>} >
+      <Route index element ={<Home/>}></Route>
+      <Route path = 'shop' element ={<Shop/>}></Route>
+   </Route>
   </Routes>
   );
 }
--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 import Directory from "../../directory/directory.component";
 
 const Home = () => {
@@ -31,7 +33,12 @@ const Home = () => {
   ]
   
   
-  return <Directory categories={categories} />;
+  return (
+    <div>
+    <Outlet/>
+    <Directory categories={categories} />;
+  </div>
+  )
 };
 
 export default Home;
