e2204a7efc383b9c6aa59a9195bb68387d318a77
SparkFlowGt <scinteiestefan@gmail.com>
2024-03-13 04:21:44 +0200

    navi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { Routes, Route,Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/routes/home/home.component";
+import Navigation from "./components/routes/home/navigation/navigation.component";
 
-const Navigation = () => {
-   return (
-      <div>
-         <div>
-            <h1> I am the navigation bar </h1>
-         </div>
-         <Outlet/>
-      </div>
-   )
-}
 
 const Shop = () => {
    return <h1> I am the shop page</h1>;
